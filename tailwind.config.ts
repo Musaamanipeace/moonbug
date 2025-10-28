553846e95239bedcef932afe5f98cfbf3e1ea8d9
Amani Moses <amanimosespeace@gmail.com>
2025-10-28 16:48:47 +0000

    use a different font for the moonplayer banner. can you make it pixel ar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Literata', 'serif'],
-        headline: ['Literata', 'serif'],
+        headline: ['"Source Code Pro"', 'monospace'],
         code: ['"Source Code Pro"', 'monospace'],
       },
       colors: {
