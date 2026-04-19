0421fc4f62b9b867f257d4dbd18d7b2a7a1468aa
Amani Moses <amanimosespeace@gmail.com>
2026-04-19 08:41:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8288583219-9ee8b",
+  "appId": "1:945501927499:web:7a5323025ec4e68f56f5ce",
+  "apiKey": "AIzaSyBxO6Zo55T6xAerw2q4iPCq6QAaZ6WZgmo",
+  "authDomain": "studio-8288583219-9ee8b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "945501927499"
+};
