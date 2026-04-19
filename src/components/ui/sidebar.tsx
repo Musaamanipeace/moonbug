17564963adaa3bfe2aca5a4203c47c9f890acd93
Your Name <amanimosespeace@gmail.com>
2026-04-19 17:03:43 +0000

    Check again

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -264,7 +264,7 @@ Sidebar.displayName = 'Sidebar';
 const SidebarTrigger = React.forwardRef<
   React.ElementRef<typeof Button>,
   React.ComponentProps<typeof Button>
->(({className, onClick, ...props}, ref) => {
+>(({className, onClick, children, ...props}, ref) => {
   const {toggleSidebar} = useSidebar();
 
   return (
@@ -272,16 +272,14 @@ const SidebarTrigger = React.forwardRef<
       ref={ref}
       data-sidebar="trigger"
       variant="ghost"
-      size="icon"
-      className={cn('h-7 w-7', className)}
+      className={cn(className)}
       onClick={(event) => {
         onClick?.(event);
         toggleSidebar();
       }}
       {...props}
     >
-      <PanelLeft />
-      <span className="sr-only">Toggle Sidebar</span>
+      {children || <PanelLeft />}
     </Button>
   );
 });
